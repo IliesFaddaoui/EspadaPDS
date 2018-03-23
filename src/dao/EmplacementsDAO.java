9c2b752366235b41ee887efa093eb2d9d395c9f3
VenomOcelot <faddaoui.i@gmail.com>
2018-03-23 01:03:48 +0100

    add dao emplacements + test functions

--- src/dao/EmplacementsDAO.java
+++ src/dao/EmplacementsDAO.java
@@ -1,4 +1,53 @@
 package dao;
+import pojo.Emplacements;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class EmplacementsDAO {
+public class EmplacementsDAO extends DAO<Emplacements> {
+    public EmplacementsDAO(Connection conn){
+        super(conn);
+    }
+
+
+    public boolean create(Emplacements obj) {
+
+        try{
+            int result = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY).executeUpdate("INSERT INTO Emplacements(idEmplacement,localisation, superficieE,categorie, tauxOccupation) values ("+obj.getIdEmplacement()+",\""+obj.getLocalisation()+"\","+obj.getSuperficieE()+",\""+obj.getCategorie()+"\","+obj.getTauxOccupation()+")");
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public boolean delete(Emplacements obj) {
+        try {
+            int result = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY).executeUpdate("DELETE from Emplacements WHERE idEmplacement=" + obj.getIdEmplacement());
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public boolean update(Emplacements obj){
+            try {
+                int result = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY).executeUpdate("UPDATE Emplacements SET localisation='" + obj.getLocalisation() + "', superficieE=" + obj.getSuperficieE() + ",categorie='" + obj.getCategorie() + "',tauxOccupation=" + obj.getTauxOccupation() + " where idEmplacement=" + obj.getIdEmplacement());
+                return true;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+    public Emplacements find(int idEmplacement) {
+        try{
+            ResultSet result = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT idEmplacement, localisation, superficieE, categorie, tauxOccupation FROM Emplacements Where idEmplacement="+ idEmplacement);
+            while(result.next()){
+                Emplacements mag = new Emplacements(result.getInt("idEmplacement"),result.getString("localisation"), result.getInt("superficieE"), result.getString("categorie"), result.getFloat("tauxOccupation"));
+                return mag;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
--- src/test/Test.java
+++ src/test/Test.java
@@ -5,15 +5,24 @@ import java.sql.SQLException;
 import connexion.Database;
 import dao.MagasinsDAO;
 import pojo.Magasins;
+import dao.EmplacementsDAO;
+import pojo.Emplacements;
 public class Test {
 
 	public static void main(String[] args) throws SQLException {
 		Connection connexion = Database.getConnection();
-		MagasinsDAO m1Dao = new MagasinsDAO(connexion);
+		//MagasinsDAO m1Dao = new MagasinsDAO(connexion);
 		//System.out.print(m1Dao.find(2).getNom());
-		Magasins m1 = new Magasins(2, "auchan", 120, 2);
-		m1Dao.create(m1);
-		System.out.print(m1Dao.find(2).getNom());
+		//Magasins m1 = new Magasins(2, "auchan", 120, 2);
+		//m1Dao.create(m1);
+		//System.out.print(m1Dao.find(2).getNom());
+		EmplacementsDAO e1DAO = new EmplacementsDAO(connexion);
+		Emplacements e1 = new Emplacements(1,"Bas-gauche",150, "A", 82);
+		e1DAO.create(e1);
+		System.out.println("ancienne loca: "+e1DAO.find(1).getLocalisation());
+		e1.setLocalisation("Haut-droite");
+		e1DAO.update(e1);
+		System.out.println("nouvelle loca: "+ e1DAO.find(1).getLocalisation());
 
 	}
 
