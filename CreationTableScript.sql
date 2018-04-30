2f969e11cea3c0e99fbf116b80a7a4b84eb6b36d
IliesFaddaoui <33630011+IliesFaddaoui@users.noreply.github.com>
2018-04-30 13:44:27 +0200

    Add sql creation tables script

---
+++ CreationTableScript.sql
@@ -0,0 +1,44 @@
+CREATE table Client (
+idClient integer not null,
+clientName varchar(30) not null,
+clientSurname varchar(30) not null,
+birthdate date not null,
+phone varchar(13) ,
+address varchar(100),
+gender varchar(5),
+PRIMARY KEY(idClient)
+);
+
+CREATE table TypicalProfile (
+idTypicalProfile integer not null,
+ProfileName varchar(30),
+PRIMARY KEY (idTypicalProfile));
+
+CREATE table Product (
+idProduct integer not null,
+productReference varchar(50) not null,
+stock integer not null,
+price integer not null,
+keyword varchar(20) not null,
+PRIMARY KEY(idProduct)
+);
+
+CREATE table PurchaseHistory (
+idPurchaseHistory integer not null,
+idProduct integer not null,
+idClient integer not null,
+purchaseDate date not null,
+Quantity integer not null,
+PRIMARY KEY(idProduct),
+foreign key(idProduct) references Product(idProduct),
+foreign key(idClient) references Client(idClient) 
+);
+
+CREATE table EtablishedProfile (
+idEtablishedProfile integer not null,
+idTypicalProfile integer not null,
+idClient integer not null,
+PRIMARY KEY(idEtablishedProfile),
+foreign key(idTypicalProfile) references TypicalProfile(idTypicalProfile),
+foreign key(idClient) references Client(idClient) 
+);
