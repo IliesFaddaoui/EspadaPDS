b5a664929dc978f686ca6e0b564f722a60cfaf45
VenomOcelot <faddaoui.i@gmail.com>
2018-05-09 20:23:51 +0200

    update client connection view

--- FlowingScript.sql
+++ FlowingScript.sql
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
---flowing database script
---please don't modify existing line, just add yours
-Insert into Client value(1,'ilies123','ilies123', 'faddaoui', 'ilies', '0618622953', '5 rue a', 'male');
-Insert into Client value(2,'bertrand123','bertrand123', 'renard', 'bertrand','0618156154', '4 rue b', 'male');
-
 Insert into KeyWord value (1, 'consoleJeuxVideo');
 Insert into KeyWord value (2, 'BandeDessineeAdulte');
 Insert into KeyWord value (3, 'jeuxvideoAdulte');
@@ -13,33 +7,30 @@ Insert into KeyWord value (6, 'Smartphone');
 Insert into KeyWord value (7,'AppareilPhoto');
 Insert into KeyWord value (8, 'EcouteurMusique');
 Insert into KeyWord value (9, 'Ecran');
+Insert into KeyWord value (10, 'Fast-Food');
+Insert into KeyWord value (11, 'equipement randonnÃ©e');
+Insert into KeyWord value (12, 'beautÃ©');
 
 Insert into Product value(1, 'Television 50cm Asus LG152', 450, 9);
 Insert into Product value(2, 'Nintendo Switch', 299, 1);
+Insert into Product value(3, 'Sandwich jambon beurre deau-nah', 3, 10);
+Insert into Product value(4, 'sac Ã  dos randonnÃ©e', 20, 11);
+Insert into Product value(5, 'parfum Dior nÂ°10', 99, 12 );
+
+Insert into Client value(1,'ilies123','ilies123', 'faddaoui', 'ilies', '0618622953', '5 rue a', 'm');
+Insert into Client value(2,'bertrand123','bertrand123', 'renard', 'bertrand','0618156154', '4 rue b', 'm');
+Insert into Client value(3,'robert123','robert123', 'robert', 'redford','06235288', '6 rue b', 'm');
+Insert into Client value(4,'bea123','bea123', 'beatrice', 'solide','054175120', '8 rue 8', 'f');
+Insert into Client value(5,'jeanne123','jeanne123', 'jeanne', 'rapouille','026522311', '8 rue dart', 'f');
+Insert into Client value(6,'farid123','farid123', 'farid', 'benarfa','09175120', '6 rue courneuse', 'm');
+Insert into Client value(7,'fatou123','fatou123', 'fatoumata', 'benarfa','09175120', '6 rue courneuse', 'f');
+Insert into Client value(8,'Wong123','Wong123', 'Wong', 'shihao','0615263659', '8 rue lajoulie', 'f');
+Insert into Client value(9,'adam123','adam123', 'adamska', 'Shalaskala','0501020302', '8 rue 4 mai', 'm');
+Insert into Client value(10,'jojo123','jojo123', 'john', 'doe','0502030214', '8 rue zero', '?');
 
 Insert into PurchaseHistory value (1, 1, 1, date(sysdate()), 1);
 Insert into PurchaseHistory value (2, 2, 1, date(sysdate()), 1);
-=======
---flowing database script
---please don't modify existing line, just add yours
-Insert into Client value(1,'ilies123','ilies123', 'faddaoui', 'ilies', '0618622953', '5 rue a', 'male');
-Insert into Client value(2,'bertrand123','bertrand123', 'renard', 'bertrand','0618156154', '4 rue b', 'male');
 
-Insert into KeyWord value (1, 'consoleJeuxVideo');
-Insert into KeyWord value (2, 'BandeDessineeAdulte');
-Insert into KeyWord value (3, 'jeuxvideoAdulte');
-Insert into KeyWord value (4, 'MusiqueRap');
-Insert into KeyWord value (5, 'MusiqueRock');
-Insert into KeyWord value (6, 'Smartphone');
-Insert into KeyWord value (7,'AppareilPhoto');
-Insert into KeyWord value (8, 'EcouteurMusique');
-Insert into KeyWord value (9, 'Ecran');
-
-Insert into Product value(1, 'Television 50cm Asus LG152', 100, 450, 9);
-Insert into Product value(2, 'Nintendo Switch', 100, 299, 1);
-
-Insert into PurchaseHistory value (1, 1, 1, date(sysdate()), 1);
-Insert into PurchaseHistory value (2, 2, 1, date(sysdate()), 1);
 
 Insert into Magasin value (1, 'Courir', 'Sport',875);
 Insert into Magasin value (2, 'Adidas', 'Sport',700);
@@ -49,10 +40,10 @@ Insert into Magasin value (5, 'Go sport', 'Sport',888);
 Insert into Magasin value (6, 'Intersport', 'Sport',600);
 Insert into Magasin value (7, 'Decathlon', 'Sport',1400);
 Insert into Magasin value (23, 'JDsport', 'Sport',943);
-Insert into Magasin value (8, 'Fnac', 'Multimédia',963);
-Insert into Magasin value (9, 'Darty', 'Multimédia',1065);
-Insert into Magasin value (10, 'Boulanger', 'Multimédia',875);
-Insert into Magasin value (22, 'Apple', 'Multimédia',747);
+Insert into Magasin value (8, 'Fnac', 'Multimï¿½dia',963);
+Insert into Magasin value (9, 'Darty', 'Multimï¿½dia',1065);
+Insert into Magasin value (10, 'Boulanger', 'Multimï¿½dia',875);
+Insert into Magasin value (22, 'Apple', 'Multimï¿½dia',747);
 Insert into Magasin value (11, 'H&M', 'Vetement',1000);
 Insert into Magasin value (12, 'Devred', 'Vetement',875);
 Insert into Magasin value (13, 'Zara', 'Vetement',1176);
@@ -64,4 +55,3 @@ Insert into Magasin value (18, 'Mango', 'Vetement',765);
 Insert into Magasin value (19, 'New look', 'Vetement',654);
 Insert into Magasin value (20, 'Primark', 'Vetement',1200);
 Insert into Magasin value (21, 'Jules', 'Vetement',624);
->>>>>>> d1720605911cc802aee00956b2c03442b66caec1
--- src/vue/ClientProfileView.java
+++ src/vue/ClientProfileView.java
@@ -2,12 +2,15 @@ package vue;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class ClientProfileView extends JFrame {
 
     private JLabel connectionText = new JLabel("Please log in to see your profile: ");
     private JLabel espada = new JLabel("PhyGit Mall");
     private Font police = new Font("Arial", Font.BOLD, 14);
+    private Font policeEspada = new Font("Arial", Font.BOLD, 28);
     private JLabel labelConnection;
     public JTextField jtfPseudo = new JTextField("pseudo");
     public JTextField jtfPassword = new JTextField("password");
@@ -19,13 +22,66 @@ public class ClientProfileView extends JFrame {
     public ClientProfileView(){
         this.setTitle("PhyGit Mall: My Profile");
         this.setSize(600,600);
+        this.setResizable(false);
+        espada.setFont(policeEspada);
 
+        jtfPseudo.getFont().deriveFont(Font.ITALIC);
+        jtfPseudo.setForeground(Color.gray);
+        jtfPseudo.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+            @Override
+            public void mousePressed(MouseEvent e) {}
+            @Override
+            public void mouseExited(MouseEvent e) {}
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                JTextField t1 = ((JTextField)e.getSource());
+                t1.setText("");
+                t1.getFont().deriveFont(Font.PLAIN);
+                t1.setForeground(Color.black);
+                t1.removeMouseListener(this);
+            }
+        });
+        jtfPassword.getFont().deriveFont(Font.ITALIC);
+        jtfPassword.setForeground(Color.gray);
+        jtfPassword.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+            @Override
+            public void mousePressed(MouseEvent e) {}
+            @Override
+            public void mouseExited(MouseEvent e) {}
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                JTextField t2 = ((JTextField)e.getSource());
+                t2.setText("");
+                t2.getFont().deriveFont(Font.PLAIN);
+                t2.setForeground(Color.black);
+                t2.removeMouseListener(this);
+            }
+        });
         JPanel top = new JPanel();
         JPanel west = new JPanel();
+        JPanel east = new JPanel();
         JPanel center = new JPanel();
         JPanel bot = new JPanel();
+
+        top.setPreferredSize(new Dimension(100,250));
+        center.setPreferredSize(new Dimension(100,60));
+        west.setPreferredSize(new Dimension(100,600));
+        east.setPreferredSize(new Dimension(100,600));
+        bot.setPreferredSize(new Dimension(100,250));
+
         GridLayout layoutCenter = new GridLayout(2,1);
+        GridLayout layoutTop = new GridLayout(1,2);
+        top.setLayout(layoutTop);
         center.setLayout(layoutCenter);
+        top.add(espada);
         top.add(connectionText);
         container.setLayout(new BorderLayout());
         center.add(jtfPseudo);
@@ -36,12 +92,14 @@ public class ClientProfileView extends JFrame {
         container.add(top, BorderLayout.NORTH);
         container.add(center, BorderLayout.CENTER);
         container.add(bot, BorderLayout.SOUTH);
+        container.add(east, BorderLayout.EAST);
+        container.add(west, BorderLayout.WEST);
         this.setContentPane(container);
-        this.pack();
         this.setVisible(true);
 
 
     }
+
     public static void main(String[] args){
         ClientProfileView p1 = new ClientProfileView();
     }
