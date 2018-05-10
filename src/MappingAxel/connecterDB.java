15e5d575d2606743e33c4bba87a2a02c47aa057b
Kapitax <axel921@hotmail.fr>
2018-05-10 15:15:20 +0200

    Update of the algorithm

--- src/MappingAxel/connecterDB.java
+++ src/MappingAxel/connecterDB.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import com.mysql.jdbc.Statement;
@@ -31,9 +32,10 @@ public class connecterDB {
 		ArrayList<Emplacements> empls = getAllEmplacements(idEmpls, empldao);*/
 		
 		Date aujourdhui = new Date();
+		SimpleDateFormat formater = null;
+		formater = new SimpleDateFormat("dd-MM-yy");
 		
-		DateFormat MediumDateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
-		//System.out.println(MediumDateFormat.format(aujourdhui));
+		
 		// Request to select attribut of emplacement on the DB
 		Emplacements empl1 = empldao.find(1);
 		Emplacements empl2 = empldao.find(2);
@@ -50,6 +52,7 @@ public class connecterDB {
 
 		//Magazins mag5 = magdao.findMax();
 		int i = 0;
+		int j = 1;
 		Emplacements empl10 = empldao.findMax(i);
 	
 		
@@ -57,26 +60,40 @@ public class connecterDB {
 	//	System.out.println(mag5.getIdMagasin());
 		// Test d'insertion dans la table Occupation
 		
-		for(i = 1; i < 20; i+=2) {
-			int j=2;
-			j+=2;
+		for(i = 1; i < 10; i++) {
+	
+			j++;
 		Magazins mag1 = magdao.find(i);
 		Magazins mag2 = magdao.find(j);
 		Emplacements empl0 = empldao.find(i); 
+		Emplacements empl01 = empldao.find(j);
 		if (mag1.getMagasinType() != mag2.getMagasinType() && mag1.getMagasinSuperficie() < empl0.getSuperficie())
 			try {
 				java.sql.Statement stmt = con.createStatement();
-				String sql = "INSERT INTO Occupation(idMagasin, idEmplacement, dateEntree) values ("+mag1.getIdMagasin()+","+ empl0.getIdEmplacement()+", '"+MediumDateFormat.format(aujourdhui)+"')";
+				String sql = "INSERT INTO Occupation(idMagasin, idEmplacement, dateEntree) values ("+mag1.getIdMagasin()+","+ empl0.getIdEmplacement()+", '"+formater.format(aujourdhui)+"')";
 				stmt.executeUpdate(sql);
+				
+				System.out.println(j);
+				
 				}catch(SQLException e) {
 					e.printStackTrace();
 				}
+		else if (mag1.getMagasinType() == mag2.getMagasinType() && mag1.getMagasinSuperficie() < empl01.getSuperficie())
+		try {
+			java.sql.Statement stmt = con.createStatement();
+			String sql = "INSERT INTO Occupation(idMagasin, idEmplacement, dateEntree) values ("+mag1.getIdMagasin()+","+ empl01.getIdEmplacement()+", '"+formater.format(aujourdhui)+"')";
+			stmt.executeUpdate(sql);
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
 		else 
-			System.out.println("Regarde bien khoya y'a une erreur");
+		System.out.println("Regarde bien khoya y'a une erreur : " + mag1.getIdMagasin());
+		}
+
 		//System.out.println("Magasin1 = "+mag1.getIdMagasin());
 	//	System.out.println("Emplacement1 = "+ empl1.getIdEmplacement());
 		}
-	}
+	
 	
 	public static Connection BDD() {
 		try {
