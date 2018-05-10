8bb927fa49ea58e1ad90168e7aefaed23b8f3071
Anaximandro Biai <anaximandrobiai@gmail.com>
2018-05-11 00:00:00 +0100

    Update analyseAnax package

---
+++ src/analyseAnax/ViewMain.java
@@ -0,0 +1,13 @@
+package analyseAnax;
+
+
+/**
+ * @author ilies
+ * @version 1.2
+ * Class which instances the main client frame
+ */
+public class ViewMain {
+    public static void main(String[]args){
+        ChiffreDaffairesView p1 = new ChiffreDaffairesView();
+    }
+}
