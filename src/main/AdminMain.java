3e218be27bc279e093365087947625946bbb46a0
VenomOcelot <faddaoui.i@gmail.com>
2018-05-15 23:30:33 +0200

    add main class for admin

---
+++ src/main/AdminMain.java
@@ -0,0 +1,43 @@
+package main;
+
+import analyseAnax.ChiffreDaffairesView;
+import mappingAxel.MappingView;
+import mappingAxel.connecterDB;
+import redevanceDan.FactureView;
+import stockAbdessamad.vue.ClientReturnOrderView;
+import stockAbdessamad.vue.InventoryListHistoryView;
+import stockAbdessamad.vue.SaleProductView;
+import stockAbdessamad.vue.StockDeliveryEntryView;
+
+public class AdminMain {
+    public static void main(String[]args){
+        ChiffreDaffairesView p1 = new ChiffreDaffairesView();
+        connecterDB bd = new connecterDB();
+        MappingView m1 = new MappingView(bd);
+        FactureView rv = new FactureView();
+        try {
+            ClientReturnOrderView frame = new ClientReturnOrderView();
+            frame.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            InventoryListHistoryView frame = new InventoryListHistoryView();
+            frame.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            SaleProductView frame = new SaleProductView();
+            frame.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            StockDeliveryEntryView frame = new StockDeliveryEntryView();
+            frame.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/ClientMain.java
@@ -0,0 +1,9 @@
+package main;
+
+import vue.ClientProfileView;
+
+public class ClientMain {
+    public static void main(String[]args){
+        ClientProfileView p1 = new ClientProfileView();
+    }
+}
