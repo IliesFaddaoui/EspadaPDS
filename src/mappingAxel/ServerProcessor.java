d40498cf2bacbc4df950c950fec93b74455f0acd
Kapitax <18164982+AxelNkl@users.noreply.github.com>
2018-06-27 23:51:33 +0200

    Update

--- src/mappingAxel/ServerProcessor.java
+++ src/mappingAxel/ServerProcessor.java
@@ -15,12 +15,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-//import com.mysql.jdbc.Statement;
-
+import javax.swing.JOptionPane;
 import com.google.gson.Gson;
-
 import connexion.PoolDeConnexion;
-
+import javax.swing.JOptionPane;
 
 public class ServerProcessor {
 
@@ -34,49 +32,43 @@ public class ServerProcessor {
 		
 		
 
-		// Mettre en place socket + seria + thread 
+	
 		try {
-			fw = new FileWriter("Mapping1.txt");
+			fw = new FileWriter("L.txt");
 			
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		PrintWriter pw = new PrintWriter(fw);
-		
 		Date aujourdhui = new Date();
 		SimpleDateFormat formater = null;
-		formater = new SimpleDateFormat("yy-MM-dd");
-		
+		formater = new SimpleDateFormat("dd-MM-yy");
 		MagasinsDAO magdao = new MagasinsDAO(con);
 		
 		int i = 0;
 		int j = 1;
-		
-		OccupationDAO occpdao = new OccupationDAO(con);
 
 		for(i = 1; i < 31; i++) {
-			
-		// Modifier critères attribution emplacement 
-		// Add Chiffre d'affaire et zone privilégié 
-			
+		
 		Magazins mag1 = magdao.find(i);
-		Magazins mag2 = magdao.find(j);
+
 		if (mag1 == null)
 			continue ;
 
 		Emplacements empl0 = empldao.find(i); 
 		Emplacements empl01 = empldao.find(i);
 		 j++;
+		 
 		if (mag1.getMagasinSuperficie() < empl0.getSuperficie())
 			try {
 				java.sql.Statement stmt = con.createStatement();
 				String sql = "INSERT INTO Occupation(idMagasin, idEmplacement, dateEntree) values ("+mag1.getIdMagasin()+","+ empl0.getIdEmplacement()+", '"+formater.format(aujourdhui)+"')";
 				stmt.executeUpdate(sql);
-				pw.println("Le magasin "+ mag1.getMagasinName()+" a été placé �  l'emplacement "+ empl0.getLocalisation());
+				pw.println("Le magasin "+ mag1.getMagasinName()+" a été placé à  l'emplacement "+ empl0.getLocalisation());
 				
 				}catch(SQLException e) {
-					pw.println("Le magasin "+mag1.getMagasinName()+" �  déj�  été placé �  l'emplacement "+empl0.getLocalisation());
+					pw.println("Le magasin "+mag1.getMagasinName()+" Ã  déjà  été placé à l'emplacement "+empl0.getLocalisation());
 				}
 			
 		else if (mag1.getMagasinSuperficie() < empl01.getSuperficie())
@@ -84,80 +76,34 @@ public class ServerProcessor {
 			java.sql.Statement stmt = con.createStatement();
 			String sql = "INSERT INTO Occupation(idMagasin, idEmplacement, dateEntree) values ("+mag1.getIdMagasin()+","+ empl01.getIdEmplacement()+", '"+formater.format(aujourdhui)+"')";
 			stmt.executeUpdate(sql);
-			pw.println("Le magasin "+ mag1.getMagasinName()+" a été placé �  l'emplacement "+ empl01.getCategorie());
+			pw.println("Le magasin "+ mag1.getMagasinName()+" a été placé à  l'emplacement "+ empl01.getCategorie());
 			
 		}catch(SQLException e) {
-				pw.println("Le magasin "+mag1.getMagasinName()+" a déj�  été placé �  l'emplacement "+empl01.getLocalisation());
+				pw.println("Le magasin "+mag1.getMagasinName()+" a déjà  été placé à  l'emplacement "+empl01.getLocalisation());
 			}
 		else 
 		pw.println("Le magasin " + mag1.getMagasinName()+" n'a pas pu être placé");
 		}
 		pw.close();
-	
-	//}
-	
-	// Pour nouvelle attribution Magasin Emplacement 
-	//public void newStore(String magasin, String emplacement) {
-		 Socket sock = null;
-	 PrintWriter writer = null;
-//	 BufferedInputStream reader=null;
-	 PoolDeConnexion connection;
-	 //writer = new PrintWriter(sock.getOutputStream());
-	//	reader = new BufferedInputStream(sock.getInputStream());
-		//Gson gson = new Gson();
-//	Connection con = BDD();
-//		Date aujourdhui = new Date();
-	//	SimpleDateFormat formater = null;
-		formater = new SimpleDateFormat("dd-MM-yy");
-		MagasinsDAO magdao1 = new MagasinsDAO(con);
-		//EmplacementsDAO empldao = new EmplacementsDAO(con);
-		try {
-			ServerSocket s = new ServerSocket(500);
-			System.out.println("test1");
-		    Socket socket = s.accept();    
-		    System.out.println("test2");
-		    BufferedReader plec = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		    PrintWriter pred = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
-		    Gson gson = new Gson();
+		JOptionPane jop1;
+		jop1 = new JOptionPane();
+		jop1.showMessageDialog(null, "Serveur lancé", "Information", JOptionPane.INFORMATION_MESSAGE);
 
-		        while(true) {
-		            String str = plec.readLine();
-		            System.out.println("--" + str +"--");
-		       
-		         Occupation e1 = gson.fromJson(str, Occupation.class);
-		          System.out.println("magasin = " +e1.magasin + " emplacement = " +e1.emplacement);
-		           // String str2 = plec.readLine();
-		            if (str.equals("END")) break;
-		            //if (str2.equals("END")) break;
-		      //     Emplacements e1 = gson.fromJson(str, Emplacements.class);
-		            System.out.println(str);
-		         //   System.out.println(str2);
-		           pred.println(str);
-		           // pred.println(str2);
-		        
-				
-			Magazins magname = magdao1.findName(e1.magasin);
-		Emplacements emp = empldao.findName(e1.emplacement);
-		java.sql.Statement stmt = con.createStatement();
-			String sql = "INSERT INTO Occupation(idMagasin, idEmplacement, dateEntree) values ("+magname.getIdMagasin()+","+ emp.getIdEmplacement()+", '"+formater.format(aujourdhui)+"')";
-			System.out.println(sql);
-			stmt.executeUpdate(sql);
-			System.out.println("Le magasin "+ magname.getMagasinName() +" a �t� plac� �  l'emplacement "+ emp.getLocalisation());
-		         }
-		      plec.close();
-		        pred.close();
-		        socket.close();
-			
-			
-		        } catch (Exception e) {
+		Socket sock = null;
+		PrintWriter writer = null;
+		PoolDeConnexion connection;
+	
+		try {
+			ServerSocket s = new ServerSocket(5001);
+			while (true) {
+				Socket clientSocket = s.accept();
+				new Thread(new CreateNewStore(clientSocket, con)).start();
+			}
+
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
-		System.out.println("Il y a eu une erreur");
 		}}
 
-	class Occupation {
-		String magasin;
-		String emplacement;
-	}
 	
 	public static Connection BDD() {
 		try {
@@ -167,11 +113,71 @@ public class ServerProcessor {
 			String user="root";
 			String password="root";
 			Connection con=DriverManager.getConnection(url, user, password);
-			System.out.println("Connexion établie");
+			System.out.println("Connexion Ã©tablie");
 			return con;
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
+}
+
+	class Occupation1 {
+	String magasin;
+	String emplacement;
+}
+
+	class CreateNewStore implements Runnable {
+	Socket clientSocket;
+	Connection con;
+	public CreateNewStore(Socket clientSocket, Connection con) {
+		this.clientSocket = clientSocket;
+		this.con = con;
+	}
+	@Override
+	public void run() {
+		
+		try {
+			BufferedReader plec = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			PrintWriter pred = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())),true);
+			Gson gson = new Gson();
+
+			while(true) {
+				String str = plec.readLine();
+				System.out.println("--" + str +"--");
+
+				Occupation1 e1 = gson.fromJson(str, Occupation1.class);
+				System.out.println("magasin = " +e1.magasin + " emplacement = " +e1.emplacement);
+				if (str.equals("END")) break;
+				System.out.println(str);
+				pred.println(str);
+				MagasinsDAO magdao1 = new MagasinsDAO(con);
+
+				Date aujourdhui = new Date();
+				SimpleDateFormat formater = null;
+				formater = new SimpleDateFormat("yy-MM-dd");
+
+
+				Magazins magname = magdao1.findName(e1.magasin);
+				EmplacementsDAO empldao = new EmplacementsDAO(con);
+				Emplacements emp = empldao.findName(e1.emplacement);
+				java.sql.Statement stmt;
+
+				stmt = con.createStatement();
+
+				String sql = "INSERT INTO Occupation(idMagasin, idEmplacement, dateEntree) values ("+magname.getIdMagasin()+","+ emp.getIdEmplacement()+", '"+formater.format(aujourdhui)+"')";
+				System.out.println(sql);
+				stmt.executeUpdate(sql);
+
+				System.out.println("Le magasin "+ magname.getMagasinName() +" a été placé à  l'emplacement "+ emp.getLocalisation());
+			}
+		}
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+		}
+
+		
+	}
+	
 }
